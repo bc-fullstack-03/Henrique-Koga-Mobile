a92f9a387dfad92b0a2d71da39e90047132372fc
Henrique Hideaki <hideakikoga.hk@gmail.com>
2023-05-30 23:08:19 -0300

    Tela incial

--- App.tsx
+++ App.tsx
@@ -3,32 +3,65 @@ import SignUp from './src/Screens/SignUp/index'
 import Friends from './src/Screens/Friends/index'
 import Profile from './src/Screens/Profile/index'
 import * as React from "react";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Home from './src/Screens/Home/index'
+import { THEME } from './src/theme';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter"
+import Loading from './src/Loading';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function App(){
-  const IsLoggedIn = false;
-  
-  return (
-    <NavigationContainer>
-
-        {!IsLoggedIn ? (<Stack.Navigator>
-            <Stack.Screen name='Login' component={Login}></Stack.Screen>
-            <Stack.Screen name='SignUp' component={SignUp}></Stack.Screen>        
-        </Stack.Navigator>) : (
-            <Tab.Navigator>
-              <Tab.Screen name='Home' component={Home} ></Tab.Screen>  
-              <Tab.Screen name='Friends' component={Friends}></Tab.Screen> 
-              <Tab.Screen name='Profile' component={Profile}  ></Tab.Screen>  
-            </Tab.Navigator>
-      )}
-    </NavigationContainer>
-  )
+const AppTheme = {
+  ...DefaultTheme,
+  dark:true,
+  colors: {
+    ...DefaultTheme.colors,
+    background: THEME.COLORS.BACKGROUND_900
+  }
 }
+
+function App(){
+  const isLoggedIn = false;
+  
+  const[ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  })
+
+  if (!fontsLoaded){
+    return <Loading navigation={undefined} />
+  }
+
+  return (
+    
+    <NavigationContainer theme={AppTheme}>
+        {!isLoggedIn ? (
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+              statusBarStyle: "light"
+              }}
+          >
+                <Stack.Screen name='Login' component={Login}></Stack.Screen>
+                <Stack.Screen name='SignUp' component={SignUp}></Stack.Screen>        
+          </Stack.Navigator>) : (
+              <Tab.Navigator>
+                <Tab.Screen name='Home' component={Home} ></Tab.Screen>  
+                <Tab.Screen name='Friends' component={Friends}></Tab.Screen> 
+                <Tab.Screen name='Profile' component={Profile}  ></Tab.Screen>  
+              </Tab.Navigator>
+      )}
+      </NavigationContainer>)}
 
 export default App;
--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,11 +1,13 @@
 import {View, Text, Button, StyleSheet, Image } from "react-native"
 import logo from "../../../assets/logo.png"
 import { styles } from "./styles"
+import { Heading } from "../../components/Heading";
 
 function Login({navigation}){
     return (
         <View style={styles.container}>
             <Image source={logo}  resizeMethod="scale"></Image>
+            <Heading title="Sysmap Parrot" subtitle='Faça login e começe a usar!'/>
         </View>
     )
 }
